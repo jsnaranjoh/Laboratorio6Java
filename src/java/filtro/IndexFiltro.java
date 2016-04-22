66f22cc35c9a9cb9dd1a1e727cec0828971bd93d
jsnar <jsnar@DESKTOP-QTBGPJL>
2016-04-22 17:53:11 -0500

    Agregado boton de cerrar sesión a los clientes

--- src/java/filtro/IndexFiltro.java
+++ src/java/filtro/IndexFiltro.java
@@ -15,7 +15,6 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import modelo.Docente;
 
 /**
  *
